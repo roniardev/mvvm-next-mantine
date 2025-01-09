4578ba55e70627f21fe937e85f4953c0d093a77f
Roni Ardiyanto <me@roniar.dev>
2025-01-09 23:31:50 +0700

    refactor(app): change eslint to biome

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,36 @@
-"use client"
+'use client';
 
-import { ColorSchemeToggle } from "../components/color-scheme-toggle";
-import { observer } from "mobx-react-lite"
-import { homeVM } from "../injector/injector";
-import { Button, Text, Flex } from '@mantine/core';
+import { Button, Flex, Text } from '@mantine/core';
+import { observer } from 'mobx-react-lite';
+import { ColorSchemeToggle } from '../components/color-scheme-toggle';
+import { homeVM } from '../injector/injector';
 
 const Page = observer(() => {
-  const count = homeVM.getCount();
-  return (
-    <Flex direction="column" gap="lg">
-      <ColorSchemeToggle />
-      <Flex direction="column" justify="center" align="center" mt="xl" gap="md">
-        <Text size="lg" fw={600}>Count: {count}</Text>
-        <Flex direction="row" gap="md">
-          <Button onClick={() => homeVM.incrementCount()}>Increment</Button>
-          <Button onClick={() => homeVM.decrementCount()}>Decrement</Button>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
-})
+	const count = homeVM.getCount();
+	return (
+		<Flex direction="column" gap="lg">
+			<ColorSchemeToggle />
+			<Flex
+				direction="column"
+				justify="center"
+				align="center"
+				mt="xl"
+				gap="md"
+			>
+				<Text size="lg" fw={600}>
+					Count: {count}
+				</Text>
+				<Flex direction="row" gap="md">
+					<Button onClick={() => homeVM.incrementCount()}>
+						Increment
+					</Button>
+					<Button onClick={() => homeVM.decrementCount()}>
+						Decrement
+					</Button>
+				</Flex>
+			</Flex>
+		</Flex>
+	);
+});
 
 export default Page;
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withVanillaExtract(nextConfig);
