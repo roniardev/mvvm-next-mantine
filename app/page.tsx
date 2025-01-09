cd0ecb0eec2ae36dfe8d7702e7cb7ab90c3cb0d7
Roni Ardiyanto <me@roniar.dev>
2025-01-09 22:40:59 +0700

    feat(home): initial simple home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import { Welcome } from "../components/Welcome/Welcome";
-import { ColorSchemeToggle } from "../components/ColorSchemeToggle/ColorSchemeToggle";
+"use client"
 
-export default function HomePage() {
+import { ColorSchemeToggle } from "../components/color-scheme-toggle";
+import { observer } from "mobx-react-lite"
+import { homeVM } from "../injector/injector";
+import { Button, Text, Flex } from '@mantine/core';
+
+const Page = observer(() => {
+  const count = homeVM.getCount();
   return (
-    <>
-      <Welcome />
+    <Flex direction="column" gap="lg">
       <ColorSchemeToggle />
-    </>
+      <Flex direction="column" justify="center" align="center" mt="xl" gap="md">
+        <Text size="lg" fw={600}>Count: {count}</Text>
+        <Flex direction="row" gap="md">
+          <Button onClick={() => homeVM.incrementCount()}>Increment</Button>
+          <Button onClick={() => homeVM.decrementCount()}>Decrement</Button>
+        </Flex>
+      </Flex>
+    </Flex>
   );
-}
+})
+
+export default Page;
